1e99aba27f58b411560c08c1a455f68a5549e50e
Vipul Shekhawat <vish092@gmail.com>
2014-04-19 15:49:30 -0400

    added methods to Game interface

--- src/edu/harvard/cs262/DistributedGame/Command.java
+++
@@ -1,5 +0,0 @@
-package edu.harvard.cs262.DistributedGame;
-
-public interface Command {
-	
-}
--- src/edu/harvard/cs262/DistributedGame/Game.java
+++ src/edu/harvard/cs262/DistributedGame/Game.java
@@ -1,5 +1,15 @@
 package edu.harvard.cs262.DistributedGame;
 
 public interface Game {
-	
+	// Get snapshot to send to client
+	public GameSnapshot getSnapshot();
+
+	// Get update to send to other servers
+	// Get full update (either game state or full log)
+	public GameUpdate getUpdate();
+	// Get partial update (log from start to current frame)
+	public GameUpdate getUpdate(long start);
+
+	// Update game state based on command argument
+	public long executeCommand(GameCommand command);
 }
