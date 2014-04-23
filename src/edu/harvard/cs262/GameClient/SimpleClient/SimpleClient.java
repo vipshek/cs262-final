c213b310cb6ca2f80439eb65b7423cb911479a06
Vipul Shekhawat <vish092@gmail.com>
2014-04-22 20:40:34 -0400

    implemented SimpleClient

---
+++ src/edu/harvard/cs262/GameClient/SimpleClient/SimpleClient.java
@@ -0,0 +1,54 @@
+package edu.harvard.cs262.GameClient.SimpleClient;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+
+import java.util.List;
+
+import edu.harvard.cs262.GameClient.GameClient;
+import edu.harvard.cs262.DistributedGame.GameCommand;
+import edu.harvard.cs262.DistributedGame.GameDisplay;
+import edu.harvard.cs262.DistributedGame.GameInputParser;
+import edu.harvard.cs262.DistributedGame.GameSnapshot;
+
+public class SimpleClient implements GameClient {
+	private GameDisplay display;
+	private GameInputParser inputParser;
+	private GameServer master;
+	private List<GameServer> slaves;
+
+	public SimpleClient(GameDisplay display, GameInputParser inputParser, GameServer master) {
+		this.display = display;
+		this.inputParser = inputParser;
+		this.master = master;
+		this.slaves = new List<GameServer>();
+	}
+
+	public void sendInput(String input) {
+		GameCommand command = this.inputParser.parseInput(input);
+		try {
+			GameSnapshot snapshot = this.master.sendCommand(command);
+		} catch (NotMasterException e) {
+			this.master = e.getMaster();
+		} catch (RemoteException e) {
+			System.out.println(e.getMessage());
+		}
+
+		this.display.render(snapshot);
+	}
+
+	public boolean addPeer(GameServer server) throws RemoteException {
+		this.slaves.add(server);
+	}
+
+	public boolean removePeer(GameServer server) throws RemoteException {
+		this.slaves.remove(server);
+	}
+
+	public boolean sendPeerList(List<GameServer> servers) throws RemoteException {
+		this.slaves = servers;
+	}
+}
