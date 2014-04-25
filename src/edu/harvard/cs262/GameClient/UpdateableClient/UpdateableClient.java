8527c8736376f5ecd6127ab432f748c094a43889
Vipul Shekhawat <vish092@gmail.com>
2014-04-24 20:45:45 -0400

    added resources directory and lanterna library; added UpdateableClient

---
+++ src/edu/harvard/cs262/GameClient/UpdateableClient/UpdateableClient.java
@@ -0,0 +1,23 @@
+package edu.harvard.cs262.GameClient.UpdateableClient;
+
+import edu.harvard.cs262.GameServer.GameServer;
+import edu.harvard.cs262.GameClient.GameClient;
+import edu.harvard.cs262.GameClient.SimpleClient.SimpleClient;
+import edu.harvard.cs262.DistributedGame.GameDisplay;
+import edu.harvard.cs262.DistributedGame.GameInputParser;
+import edu.harvard.cs262.DistributedGame.GameSnapshot;
+
+public class UpdateableClient extends SimpleClient {
+	private long currentFrame;
+
+	public UpdateableClient(GameDisplay display, GameInputParser inputParser, GameServer master) {
+		super(display,inputParser,master);
+		this.currentFrame = 0;
+	}
+
+	public void updateDisplay(GameSnapshot snapshot) {
+		if (snapshot.getFrame() > this.currentFrame) {
+			this.display.render(snapshot);
+		}
+	}
+}
