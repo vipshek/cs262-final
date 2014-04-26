e18f0b5957881578d5779241c71e7d0adf3ad2aa
Kat Zhou <kat.zhou@mail.hsa.net>
2014-04-26 19:22:41 -0400

    CommandProcessor

--- src/edu/harvard/cs262/DistributedGame/BattleshipGame/BattleshipCommandProcessor.java
+++ src/edu/harvard/cs262/DistributedGame/BattleshipGame/BattleshipCommandProcessor.java
@@ -2,8 +2,52 @@ package edu.harvard.cs262.DistributedGame.BattleshipGame;
 import edu.harvard.cs262.DistributedGame.GameCommandProcessor;
 import edu.harvard.cs262.DistributedGame.GameCommand;
 
+import java.util.List
+import java.util.concurrent.Semaphore
+
 public class BattleshipCommandProcessor implements GameCommandProcessor {
-	private BattleshipCommand command;
+	private List<Position> positions;
+	private Position output;
+	private final Semaphore available = new  Semaphore(0, true);
+
+	private class CommandUpdate implements Runnable {
+		BattleshipCommandProcessor parent;
+		public CommandUpdate(BattleshipCommandProcessor parent){
+			this.parent = parent;
+			output = null;
+		}
+
+		public void run() {
+			while(true){
+				Thread.sleep(250);
+				synchronized (positions){
+					if(output != null){
+						int xSum = 0;
+						int ySum = 0;
+						for(int i = 0; i < positions.count(); i++){
+							xSum += positions.get(i).x;
+							ySum += positions.get(i).y;
+						}
+						if(positions.count() > 0){
+							int xPos = xSum / positions.count();
+							int yPos = ySum / positions.count();
+							Position p = new Position (xPos, yPos);
+							this.parent.output = p;
+							this.parent.available.release();
+						} 
+					}
+				}
+			}
+
+		}
+	}
+
+
+	public BattleshipCommandProcessor() {
+		positions = new List<Position> ();
+		output = null;
+		new Thread(new CommandUpdate(this)).start()
+	}
 
 	// Clear previous queue of commands
 	public void startProcessor() {
@@ -12,11 +56,20 @@ public class BattleshipCommandProcessor implements GameCommandProcessor {
 
 	// Add to the processor's command queue
 	public void addCommand(GameCommand command) {
-		this.command = (BattleshipCommand) command;
+		BattleshipCommand cmd = (BattleshipCommand) command;
+		synchronized(positions) {
+			positions.add(cmd.getPosition());
+		}
 	}
 
 	// Retrieve the command decided upon by the processor
 	public GameCommand getCommand() {
-		return this.command;
+		this.available.acquire();
+		synchronized(positions){
+			this.positions.clear();
+			BattleshipCommand cmd = new BattleshipCommand(this.output.x, this.output.y);
+			this.output = null;
+			return cmd;
+		}
 	}
 }
