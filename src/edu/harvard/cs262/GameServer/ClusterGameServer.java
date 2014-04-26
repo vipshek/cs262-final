0b4abceda09e1bdd8c0b66e755bac74039efa619
Perry Green <perryagreen@gmail.com>
2014-04-26 18:13:32 -0400

    deletions

--- src/edu/harvard/cs262/GameServer/ClusterGameServer.java
+++
@@ -1,7 +0,0 @@
-package edu.harvard.cs262.GameServer;
-
-import edu.harvard.cs262.GameServer.GameServer;
-import edu.harvard.cs262.ClusterServer.ClusterServer;
-
-public interface ClusterGameServer extends GameServer, ClusterServer {
-}
