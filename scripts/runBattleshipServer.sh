5d0b3fb95861dca96f81a332d874f1a0696427a1
R.J. Aquino <rjaquino@college.harvard.edu>
2014-04-28 23:44:14 -0400

    updated Battleship script

--- scripts/runBattleshipServer.sh
+++ scripts/runBattleshipServer.sh
@@ -1,1 +1,1 @@
-java -cp '../src:../resources/*' -Djava.security.policy=server.policy edu.harvard.cs262.DistributedGame.BattleshipGame.BattleshipServer $@
+java -cp '../src:../resources/*' -Djava.security.policy=server.policy edu.harvard.cs262.DistributedGame.BattleshipGame.BattleshipClusterServer $@
