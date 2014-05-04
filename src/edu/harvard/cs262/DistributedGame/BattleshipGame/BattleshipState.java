901ae3d0dd41c3a5e6892047ccbe912a34ff782f
R.J. Aquino <rjaquino@college.harvard.edu>
2014-05-04 16:14:33 -0400

    retab

--- src/edu/harvard/cs262/DistributedGame/BattleshipGame/BattleshipState.java
+++ src/edu/harvard/cs262/DistributedGame/BattleshipGame/BattleshipState.java
@@ -2,31 +2,31 @@ package edu.harvard.cs262.DistributedGame.BattleshipGame;
 import edu.harvard.cs262.DistributedGame.GameState;
 
 class BattleshipState implements GameState {
-	private int[][] shotsBoard;
-	private ShipLocation[] shipLocations;
-	private long frameCount;
+    private int[][] shotsBoard;
+    private ShipLocation[] shipLocations;
+    private long frameCount;
     private int numHits;
     private int numMisses;
 
-	public BattleshipState(int[][] shotsBoard, ShipLocation[] shipLocations, long frameCount){
-		this.shotsBoard = shotsBoard;
-		this.shipLocations = shipLocations;
-		this.frameCount = frameCount;
+    public BattleshipState(int[][] shotsBoard, ShipLocation[] shipLocations, long frameCount){
+        this.shotsBoard = shotsBoard;
+        this.shipLocations = shipLocations;
+        this.frameCount = frameCount;
         this.numHits = 0;
         this.numMisses = 0;
-	}
+    }
 
-	public int[][] getShotsBoard() {
-		return shotsBoard;
-	}
+    public int[][] getShotsBoard() {
+        return shotsBoard;
+    }
 
-	public ShipLocation[] getShipLocations() {
-		return shipLocations;
-	}
+    public ShipLocation[] getShipLocations() {
+        return shipLocations;
+    }
 
-	public long getFrame() {
-		return frameCount;
-	}
+    public long getFrame() {
+        return frameCount;
+    }
 
     public boolean setFrame(long f) {
         if (f < frameCount)
