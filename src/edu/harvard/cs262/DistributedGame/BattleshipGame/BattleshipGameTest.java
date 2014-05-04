1ce7581a699008b8a3deb99c72cefc5a60c4b24e
R.J. Aquino <rjaquino@college.harvard.edu>
2014-05-04 17:52:00 -0400

    Stub test files

---
+++ src/edu/harvard/cs262/DistributedGame/BattleshipGame/BattleshipGameTest.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package edu.harvard.cs262.DistributedGame.BattleshipGame;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author rjaquino
+ *
+ */
+public class BattleshipGameTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
