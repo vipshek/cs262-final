d36656803734f28f3808aa3c452b2a1e5ec1b260
Vipul Shekhawat <vish092@gmail.com>
2014-05-04 22:15:09 -0400

    added compiling script

---
+++ scripts/compileTests.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# Won't work for complex hierarchies, but should work for us
+find ../tests/ -name "*.java" > sources.txt
+javac -cp '../tests:../resources/*' -g @sources.txt
+rm -f sources.txt
