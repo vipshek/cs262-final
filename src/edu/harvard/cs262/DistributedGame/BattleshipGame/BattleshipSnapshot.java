d8919e50d2064e117d5b6b0dabd9ef0446e5f778
Kat Zhou <kat.zhou@mail.hsa.net>
2014-05-04 21:49:29 -0400

    ShipLocation javadoc

--- src/edu/harvard/cs262/DistributedGame/BattleshipGame/BattleshipSnapshot.java
+++ src/edu/harvard/cs262/DistributedGame/BattleshipGame/BattleshipSnapshot.java
@@ -1,6 +1,16 @@
 package edu.harvard.cs262.DistributedGame.BattleshipGame;
 import edu.harvard.cs262.DistributedGame.GameSnapshot;
 
+/**
+ * The BattleshipSnapshot class represents the snapshot of the Battleship
+ * game.  It encompasses all information needed for clients to display the
+ * game, including the board of where shots were fired, which ships were sunk,
+ * and the current frame
+ * 
+ * @author Twitch Plays Battleship Group
+ * 
+ * @version 1.0, April 2014
+ */
 class BattleshipSnapshot implements GameSnapshot {
 	private static final long serialVersionUID = 1L;
 	
@@ -8,20 +18,51 @@ class BattleshipSnapshot implements GameSnapshot {
     private boolean[] sunkShips;
     private long frame;
 
+    /**
+     * Constructor for the BattleshipSnapshot class. Sets the snapshot's
+     * board of where shots were fired, list of sunk ships, and frame.
+     * 
+     * @param  shotsBoard  A 2-D int array that represents where shots were
+     *         fired on the board
+     *         
+     * @param  sunkShips  A boolean array that represents which ships have
+     *         been sunk
+     * 
+     * @param  A long that represents the frame number of the state
+     * 
+     * @return A BattleshipSnapshot object that encompasses the above variables
+     */
     public BattleshipSnapshot(int[][] shotsBoard, boolean[] sunkShips, long frame){
         this.shotsBoard = shotsBoard;
         this.sunkShips=sunkShips;
         this.frame = frame;
     }
 
+    /**
+     * Gets the matrix that represents what parts of the board were shot at
+     *
+     * @return A 2-D array of ints that holds whether that space on the
+     *         board was untouched, a hit, or a miss
+     */
     public int[][] getShotsBoard() {
         return shotsBoard;
     }
 
+    /**
+     * Gets the array that represents which ships were sunk
+     *
+     * @return An array of booleans that holds whether the ship at 
+     *         that index was sunk or not
+     */
     public boolean[] getSunkShips() {
         return sunkShips;
     }
 
+    /**
+     * Gets the current frame of the state
+     * 
+     * @return A long that represents the frame of the state
+     */
     public long getFrame() {
         return frame;
     }
--- src/edu/harvard/cs262/DistributedGame/BattleshipGame/ShipLocation.java
+++ src/edu/harvard/cs262/DistributedGame/BattleshipGame/ShipLocation.java
@@ -2,16 +2,41 @@ package edu.harvard.cs262.DistributedGame.BattleshipGame;
 
 import java.io.Serializable;
 
+/**
+ * The ShipLocation class represents the location of a ship on the 
+ * Battleship board.
+ *
+ * @author Twitch Plays Battleship Group
+ * 
+ * @version 1.0, April 2014
+ */
 public class ShipLocation implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
     public Position pos;
     public Direction dir;
+
+    /**
+     * Constructor for a ShipLocation object. Sets the position and direction
+     * of each ship.
+     * 
+     * @param  pos  A {@link Position} object that represents the location
+     *         where the ship starts
+     * 
+     * @param  dir  A {@link Direction} that represents the direction in which
+     *         the ship is facing
+     */
     public ShipLocation(Position pos, Direction dir) {
         this.pos = pos;
         this.dir = dir;
     }
 
+    /**
+     * Creates a string that shows whether a ship is horizontal or vertical
+     * and also its row and column position that it starts at.
+     * 
+     * @return A string that encompasses the ship's direction and position
+     */
     @Override public String toString() {
         String s = "";
 
